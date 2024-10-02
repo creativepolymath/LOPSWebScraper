8fac30e4582fe4b43f23333410bf66b7c8a8a610
Greg Whitmore <gwhitmore@gmail.com>
2024-10-01 20:18:54 -0400

    testing phase
    struggling with locally hosted ollama

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from scrape import (scrape_website, split_dom_content, clean_body_content, extract_body_content)
+from parse import parse_with_ollama
+
+
+st.title("AI Web Scraper")
+url = st.text_input("Enter Website URL:")
+
+if st.button("Scrape"):
+    st.write("scraping the site")
+    scrape_website(url)
+    
+    result = scrape_website(url)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+    
+    st.session_state.dom_content = cleaned_content
+    
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+    
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse")
+    
+    if st.button("Parse"):
+        if parse_description:
+            st.write("parsing the content")
+            
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
+            
